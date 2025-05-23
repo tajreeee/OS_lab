b7302a1c2fe252ad122fd64777781bbad4b068a0
Jannatul Ferdous Tajree <jannatultajree02@gmail.com>
2025-05-23 15:04:27 +0600

    Checking if a string is palindrome or not

---
+++ Shell_Scripting4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter a string:"
+read str
+
+rev=$(echo "$str" | rev)
+
+if [ "$str" == "$rev" ]; then
+  echo "'$str' is a palindrome."
+else
+  echo "'$str' is not a palindrome."
+fi
